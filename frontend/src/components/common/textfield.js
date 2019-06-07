42f9d9303e8da1e85973807ee093b2c8c0db5070
poonsht <poon_arsene_lupin@hotmail.com>
2019-06-07 12:06:17 +0700

    fix textfield:border-color

--- frontend/src/components/common/textfield.js
+++ frontend/src/components/common/textfield.js
@@ -1,49 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import R from '../resources/R'
+
 const WraperForm = styled.div`
   display: inline-block;
   top: 0;
 `
-const useStyles = makeStyles(theme => ({
-  textField: {
-    width: '50vw',
-    maxWidth: 352,
-    height: 56,
-    fontSize: '1.6rem',
-    borderColor: R.colors.primary,
-    paddingTop: 0,
-    marginTop: theme.spacing(0),
-    marginBottom: theme.spacing(0),
-    marginRight: theme.spacing(1),
-    backgroundColor: R.colors.white,
-  },
-  cssLabel: {
-    borderWidth: '2px',
-  },
 
-  cssOutlinedInput: {
-    fontSize: 16,
-    '&$cssFocused $notchedOutline': {
-      borderWidth: '2px',
-      borderColor: R.colors.primary,
-    },
-  },
-
-  cssFocused: {},
-
-  notchedOutline: {},
-}))
+const TextFieldEmail = styled(TextField)`
+  && {
+    width: 50vw;
+    max-width: 352px;
+    height: 56px;
+    font-size: 1.6rem;
+    padding-top: 0;
+    background-color: ${R.colors.white};
+    margin: 0 0.5em 0 auto;
+    &:hover {
+      border-width: 2px;
+      border-color: ${R.colors.pimary};
+    }
+  }
+`
 
 function TextFieldCustom({ handleTyping }) {
-  const classes = useStyles()
   const [values, setValues] = useState({
     value: '',
   })
 
   const handleChangeText = name => event => {
+    // event.persist()
     setValues({ ...values, [name]: event.target.value })
   }
 
@@ -53,33 +40,17 @@ function TextFieldCustom({ handleTyping }) {
 
   return (
     <WraperForm>
-      {' '}
-      <TextField
+      <TextFieldEmail
         id="standard-name"
         label="Email"
         type="email"
         name="email"
         autoComplete="email"
-        className={classes.textField}
         value={values.value}
         onChange={handleChangeText('value')}
         placeholder="your@verdoc.io"
         margin="normal"
         variant="outlined"
-        autoFocus
-        InputLabelProps={{
-          classes: {
-            root: classes.cssLabel,
-            focused: classes.cssFocused,
-          },
-        }}
-        InputProps={{
-          classes: {
-            root: classes.cssOutlinedInput,
-            focused: classes.cssFocused,
-            notchedOutline: classes.notchedOutline,
-          },
-        }}
       />
     </WraperForm>
   )
