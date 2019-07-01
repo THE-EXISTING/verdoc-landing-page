e2d31432f084ff3cb13eae08e3fd48b39b8196ec
poonsht <poon_arsene_lupin@hotmail.com>
2019-07-01 15:12:42 +0700

    rename textfield in subscribe page

--- frontend/src/components/common/TextFieldCustom.js
+++ frontend/src/components/common/TextFieldCustom.js
@@ -3,30 +3,37 @@ import styled from 'styled-components'
 import TextField from '@material-ui/core/TextField'
 import R from '../resources/R'
 
+const InputCustom = styled(TextField)`
+  && {
+    width: 50vw;
+    max-width: 352px;
+    height: 56px;
+    font-size: 1.6rem;
+    padding-top: 0;
+    background-color: ${R.colors.white};
+    margin: 0 0.5em 0 auto;
+
+    &:hover {
+      border-width: 2px;
+      border-color: ${R.colors.pimary};
+    }
+  }
+`
+
 const TextFieldCustom = props => {
   return (
-    <TextFieldEmail
-      id="standard-name"
-      data-testid="textfield"
-      label="Email"
-      type="email"
-      name="email"
-      autoComplete="email"
-      value={values.value}
-      onChange={handleChangeText('value')}
-      placeholder="your@verdoc.io"
+    <InputCustom
+      //   id="standard-name"
       margin="normal"
       variant="outlined"
-      error={isSubmit && !isEmail}
-      helperText={
-        isSubmit
-          ? !isEmail
-            ? textError
-            : isExist
-            ? 'You already subscribe Verdoc.'
-            : 'Thank you for subscribe Verdoc.'
-          : ''
-      }
+      data-testid="textfield"
+      label={props.label}
+      name={props.name}
+      type={props.type}
+      autoComplete={props.type}
+      //   value={values.value}
+      //   onChange={handleChangeText('value')}
+      placeholder={props.placeHolder}
     />
   )
 }
